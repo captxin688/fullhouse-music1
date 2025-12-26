b1a5a9bc25aa086994beaf27e1effdac9e0d517d
captxin688 <captxin688@gmail.com>
2025-12-26 23:07:37 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -32,10 +32,12 @@ ydl_opts = {
     'no_warnings': False, # 2. เปลี่ยนเป็น False จะได้เห็นคำเตือน
     'default_search': 'auto',
     'source_address': '0.0.0.0',
+    'cookiefile': 'cookies.txt',
     # 3. เพิ่มส่วนนี้เข้าไป (สำคัญ)
     'http_headers': {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     }
+    
 }
 
 @bot.event
